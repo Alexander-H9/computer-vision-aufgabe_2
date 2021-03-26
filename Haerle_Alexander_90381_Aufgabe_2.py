c422679f76c98fafa1a3dca64728575dec96545f
Alexander Haerle <alexander.haerle@gmx.net>
2021-03-26 12:42:58 +0100

    added threshold, erode, dilate

--- Haerle_Alexander_90381_Aufgabe_2.py
+++ Haerle_Alexander_90381_Aufgabe_2.py
@@ -4,33 +4,41 @@ import cv2 as cv
 img = cv.imread("barcode2.jpg")
 windowName = "Barcode"
 
-kernel = img.Mat.ones(2,2)
-
-cv.filter2D()
-
-# git test
+kernel = np.ones((3,3),np.uint8)    # 3x3 matrix with ones for the convolution. Easy explanation for kernels: https://towardsdatascience.com/basics-of-kernels-and-convolutions-with-opencv-c15311ab8f55
+print("Kernel shape:\n", kernel)
 
 while True:
 
     cv.imshow(windowName, img)
 
-    if cv.waitKey(1) == ord('q'):
+    k = cv.waitKey(1) & 0xFF
+
+    if k == ord('q'):
         break
 
-    elif cv.waitKey(1) == ord('x'):
-        pass
+    elif k == ord('x'):
+        print("horizontale kanten")
 
-    elif cv.waitKey(1) == ord('y'):
-        pass
+    elif k == ord('y'):
+        print("vertikale kanten")
 
-    elif cv.waitKey(1) == ord('a'):
-        pass
+    elif k == ord('a'):
+        print("glaetten")
 
-    elif cv.waitKey(1) == ord('t'):
-        pass
+    elif k == ord('t'):
+        print("threshold")
+        img_grey = cv.imread("barcode2.jpg", 0) # recommended is to use a grayscale image
+        ret, thrashold = cv.threshold(img_grey, 126, 255, cv.THRESH_BINARY)   # https://docs.opencv.org/master/d7/d4d/tutorial_py_thresholding.html
+        cv.imshow("Threshold", thrashold) 
 
-    elif cv.waitKey(1) == ord('e'):
-        pass
+    elif k == ord('e'):
+        print("erode")
+        erosion = cv.erode(img,kernel, iterations = 1)
+        cv.imshow("Erode", erosion)
 
-    elif cv.waitKey(1) == ord('d'):
-        pass
+    elif k == ord('d'):
+        print("dilate")
+        dilation = cv.dilate(img,kernel, iterations = 1)
+        cv.imshow("Dilate", dilation)
+
+cv.destroyAllWindows()
