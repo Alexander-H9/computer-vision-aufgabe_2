178360541bc301ef2a3fcaf56f9296b0161e2ae6
Alexander Haerle <alexander.haerle@gmx.net>
2021-04-01 08:15:09 +0200

    new image and improvements

--- Haerle_Alexander_90381_Aufgabe_2.py
+++ Haerle_Alexander_90381_Aufgabe_2.py
@@ -1,11 +1,11 @@
 import numpy as np
 import cv2 as cv
 
-img = cv.imread("barcode2.jpg")
-imgP = cv.imread("pirates1.jpg")
+# img = cv.imread("barcode2.jpg")
+img = cv.imread("pirates1.jpg")
 windowName = "Barcode"
 
-kernel = np.ones((5, 5),np.uint8)    # 3x3 matrix with ones for the convolution. Easy explanation for kernels: https://towardsdatascience.com/basics-of-kernels-and-convolutions-with-opencv-c15311ab8f55
+kernel = np.ones((3, 3),np.uint8)    # 3x3 matrix with ones for the convolution. Easy explanation for kernels: https://towardsdatascience.com/basics-of-kernels-and-convolutions-with-opencv-c15311ab8f55
 print("Kernel shape:\n", kernel)
 
 gauss_kernel = np.array([[1,2,1], [2,4,2], [1,2,1]], np.uint8)/16 # zum glätten geeignet
@@ -30,36 +30,42 @@ while True:
     elif k == ord('x'):
         print("horizontale kanten")
         # Wenn man Ableitungs Filter benutzt ist es wichtig zuerst das Signal zu glätten
-        blurred = blur(img, gauss_kernel)
-        horizontal = cv.filter2D(blurred, -1 , horizontal_kernel)
+        #blurred = blur(img, gauss_kernel)
+        horizontal = cv.filter2D(img, -1 , horizontal_kernel)
+        img = horizontal.copy()
         cv.imshow("Horizontale Kantenhervorhebung" , horizontal)
 
     elif k == ord('y'):
         print("vertikale kanten")
         # Wenn man Ableitungs Filter benutzt ist es wichtig zuerst das Signal zu glätten
-        blurred = blur(img, gauss_kernel)
-        vertikal = cv.filter2D(blurred, -1 , vertikal_kernel)
+        #blurred = blur(img, gauss_kernel)
+        vertikal = cv.filter2D(img, -1 , vertikal_kernel)
+        img = vertikal.copy()
         cv.imshow("Vertikale Kantenhervorhebung" , vertikal)
 
     elif k == ord('a'):
         print("glaetten")
         glatt = blur(img, gauss_kernel)
+        img = glatt.copy()
         cv.imshow("Geglaettet", glatt)
 
     elif k == ord('t'):
         print("threshold")
-        img_grey = cv.imread("barcode2.jpg", 0) # recommended is to use a grayscale image
-        ret, thrashold = cv.threshold(img_grey, 126, 255, cv.THRESH_BINARY)   # https://docs.opencv.org/master/d7/d4d/tutorial_py_thresholding.html
+        img_grey = cv.imread("pirates1.jpg", 0) # recommended is to use a grayscale image
+        ret, thrashold = cv.threshold(img_grey, 200, 255, cv.THRESH_BINARY)   # https://docs.opencv.org/master/d7/d4d/tutorial_py_thresholding.html
         cv.imshow("Threshold", thrashold) 
 
     elif k == ord('e'):
         print("erode")
+        
         erosion = cv.erode(img, kernel, iterations = 1)
+        img = erosion.copy()
         cv.imshow("Erode", erosion)
 
     elif k == ord('d'):
         print("dilate")
         dilation = cv.dilate(img, kernel, iterations = 1)
+        img = dilation.copy()
         cv.imshow("Dilate", dilation)
 
 cv.destroyAllWindows()
